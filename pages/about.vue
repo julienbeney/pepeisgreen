2f8094c4c6a827ab7e76ceb77099b61b92254acb
julienbeney <julienbeney1971@gmail.com>
2023-06-24 19:41:21 +0200

    graphic and menu updates

---
+++ pages/about.vue
@@ -0,0 +1,112 @@
+<template>
+  <div class="container bg-3fb00b h-screen w-screen p-10">
+    <div style="display: flex">
+      <button
+        @click="goBack"
+        class="font-inconsolata text-lg px-4 py-2 rounded inline-flex items-center mb-8"
+      >
+        <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+
+        <svg
+          width="80px"
+          height="80px"
+          viewBox="0 0 36.00 36.00"
+          xmlns="http://www.w3.org/2000/svg"
+          xmlns:xlink="http://www.w3.org/1999/xlink"
+          aria-hidden="true"
+          role="img"
+          class="iconify iconify--twemoji"
+          preserveAspectRatio="xMidYMid meet"
+          fill="#c5fdaf"
+          stroke="#c5fdaf"
+          stroke-width="0.00036"
+        >
+          <g id="SVGRepo_bgCarrier" stroke-width="0" />
+          <g
+            id="SVGRepo_tracerCarrier"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke="#CCCCCC"
+            stroke-width="0.144"
+          />
+          <g id="SVGRepo_iconCarrier">
+            <path
+              fill="#2c9e00"
+              d="M34 3H12.475V1.128c0-1.046-.74-1.435-1.645-.865L.69 6.652c-.905.57-.922 1.527-.038 2.127l10.215 6.931c.884.602 1.607.235 1.607-.811V13H34a2 2 0 0 0 2-2V5a2 2 0 0 0-2-2zM.024 26.184c0-.727.5-1.137 1.197-1.137H4.13c1.576 0 2.849 1.061 2.849 2.667c0 1.061-.439 1.772-1.409 2.227v.03c1.288.183 2.303 1.258 2.303 2.576c0 2.137-1.424 3.288-3.516 3.288h-3.12c-.697 0-1.212-.439-1.212-1.151v-8.5zm2.273 3.135h1.182c.742 0 1.227-.439 1.227-1.196c0-.713-.561-1.076-1.227-1.076H2.297v2.272zm0 4.516h1.788c.818 0 1.424-.47 1.424-1.318c0-.712-.545-1.197-1.606-1.197H2.297v2.515zm9.217-7.713c.258-.696.85-1.257 1.621-1.257c.805 0 1.365.53 1.621 1.257l2.971 8.243c.092.242.121.454.121.561c0 .591-.484 1-1.045 1c-.637 0-.955-.333-1.107-.788l-.453-1.424H11.03l-.455 1.409c-.15.47-.469.803-1.09.803c-.607 0-1.122-.454-1.122-1.061c0-.242.076-.424.106-.5l3.045-8.243zm.168 5.501h2.879l-1.41-4.395h-.029l-1.44 4.395zm11.378-6.758c1.106 0 3.258.363 3.258 1.696c0 .546-.379 1.016-.94 1.016c-.621 0-1.046-.53-2.318-.53c-1.879 0-2.849 1.591-2.849 3.439c0 1.803.985 3.349 2.849 3.349c1.272 0 1.788-.637 2.409-.637c.682 0 1 .682 1 1.03c0 1.455-2.288 1.788-3.409 1.788c-3.076 0-5.212-2.439-5.212-5.576c0-3.151 2.121-5.575 5.212-5.575zm4.471 1.212c0-.621.455-1.121 1.137-1.121c.651 0 1.137.424 1.137 1.121v3.273l3.727-3.97c.167-.182.455-.424.879-.424c.576 0 1.121.439 1.121 1.091c0 .393-.242.712-.742 1.212l-2.863 2.818l3.5 3.651c.363.364.637.697.637 1.152c0 .712-.562 1.045-1.183 1.045c-.44 0-.727-.258-1.151-.712l-3.924-4.243v3.864c0 .591-.455 1.091-1.137 1.091c-.651 0-1.137-.424-1.137-1.091v-8.757z"
+            />
+          </g>
+        </svg>
+      </button>
+      <h1 class="text-7xl font-bold ml-20">ABOUT</h1>
+    </div>
+    <button :onclick="openModal">weqf</button>
+    <div v-if="modalVisible" class="smooth-transition visible">This modal will be displayed according to the status when the previous one is closed or the display status is fasle</div>
+
+    <div class="bg-2c9e00 p-10 rounded-lg">
+      <p class="text-lg">
+        Green celebrates the unique cultural impact of Pepe the Frog, along with
+        the diverse and creative world of Pepe memes. Our mission is to foster a
+        community that cherishes Pepe's endearing qualities, embracing the
+        humor, artistic expression, and camaraderie that Pepe represents. As an
+        Affiliate of Green we believe that Pepe is not just a meme; it is a
+        symbol of unity, freedom of expression, and the power of internet
+        culture. Through our project, we strive to promote inclusivity, open
+        dialogue, and mutual respect among Pepe enthusiasts. We aim to provide a
+        platform where Pepe enthusiasts can connect, collaborate, and contribute
+        to the ongoing legacy of Pepe, ensuring its enduring presence in popular
+        culture. Together, let us embrace the lightheartedness and joy that Pepe
+        represents, while promoting positivity and appreciation for the diverse
+        range of Pepe memes. Green is not just a project; it is a declaration of
+        our affiliation, a testament to our commitment, and an homage to the
+        everlasting impact of Pepe the Frog.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "About",
+  data() {
+    return {
+      modalVisible: true,
+    }
+  },
+  methods: {
+    openModal(){
+      console.log(this.modalVisible)
+      this.modalVisible = !this.modalVisible
+    },
+    goBack() {
+      this.$router.go(-1);
+    },
+  },
+};
+</script>
+
+<style>
+.smooth-transition {
+  opacity: 0;
+  transition: opacity 0.5s ease-in-out;
+}
+.smooth-transition.visible {
+  opacity: 1;
+}
+.font-inconsolata,
+h1,
+h2 {
+  font-family: "Inconsolata", monospace;
+}
+.bg-2c9e00 {
+  background-color: #2c9e00;
+}
+.bg-2c9e00 {
+  background-color: #2c9e00;
+}
+.bg-3fb00b {
+  background-color: #3fb00b;
+}
+.text-3fb00b {
+  color: #3fb00b;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,427 @@
+<template>
+  <img class="pepe" src="/finalpepecolor.png" />
+  <div class="grid-container">
+    <div v-for="col in table" :onclick="col.click">
+      <a v-if="col.link !== ''" :href="col.link">
+        <div
+          :class="col.class"
+          class="square text-green-500 text-lg font-bold text-center p-10 rounded-lg"
+        >
+          <p>{{ col.text }}</p>
+        </div>
+      </a>
+      <div
+        v-else
+        :class="col.class"
+        class="square text-green-500 text-lg font-bold text-center p-10 rounded-lg"
+      >
+        <p>{{ col.text }}</p>
+      </div>
+    </div>
+  </div>
+  <div
+    v-if="ABOUT"
+    style="z-index: 100"
+    class="smooth-transition visible modal page-display fixed inset-x-0 inset-y-0 bg-gray-400 m-10 p-5 p-0-sm rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-lg shadow-xl"
+  >
+    <button
+      :onclick="openModalAbout()"
+      class="fixed top-3 right-3 bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded-full inline-flex items-center"
+    >
+      <svg
+        class="fill-current w-4 h-4"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+        />
+      </svg>
+    </button>
+    <div class="justify-center space-x-8">
+      <h1 class="text-3xl font-bold text-center m-4">Our Mission</h1>
+      <p class="text-justify leading-relaxed sm:px-6 p-8 md:px-30 xl:px-40">
+        WHAT IS #3FB00B? #3FB00B is a distinctive Proof of Involvement NFT that
+        serves multiple purposes within the $PEPE Community. This NFT not only
+        symbolizes your membership to #3FB00B but also represents your stake in
+        promoting and raising awareness about the community as a whole. The
+        captivating colors used in the #3FB00B NFT artwork are carefully chosen
+        to reflect and embody the essence and spirit of the $PEPE Community. The
+        vibrant and engaging visuals serve as a visual representation of the
+        community's energy, creativity, and shared values. By owning a #3FB00B
+        NFT, you gain exclusive utilities and opportunities.\n These utilities
+        may include early access to new features, events, or projects within the
+        $PEPE ecosystem. Additionally, as a #3FB00B NFT holder, you become
+        eligible for future utilities and benefits that are specifically
+        designed to enhance your experience and engagement within the community.
+        Being an #3FB00B NFT holder not only grants you access to exciting
+        opportunities but also positions you as an active participant and
+        supporter of the $PEPE Community. Your ownership of this unique NFT
+        demonstrates your dedication and involvement in the community's growth
+        and development. WHY WAS #3FB00B CREATED? #3FB00B was created as a
+        dedicated sub-community within the broader $PEPE ecosystem. Its primary
+        purpose is to foster an environment where passionate $PEPE enthusiasts
+        can come together and actively engage with various artists, creators,
+        and builders. By being a part of #3FB00B, members gain exclusive access
+        to curated Web3 projects, enabling them to discover and support new
+        talent and innovative initiatives. The creation of #3FB00B serves as a
+        platform for the community to explore and embrace the diverse and
+        evolving world of digital art and creativity. Through this
+        sub-community, members have the unique opportunity to connect with
+        emerging artists and creators, discovering their unique perspectives and
+        contributing to their growth and success. By focusing on curated Web3
+        projects, #3FB00B ensures that the initiatives supported are carefully
+        selected and aligned with the ethos and interests of the $PEPE
+        Community. This curated approach not only helps members find exciting
+        and innovative projects but also empowers artists and builders by
+        providing them with a dedicated audience and potential support for their
+        work. Overall, #3FB00B aims to create a thriving ecosystem within the
+        $PEPE Community, fostering collaboration, discovery, and support for
+        artists, creators, and builders in the Web3 space.
+      </p>
+    </div>
+  </div>
+  <div
+    v-if="MISSION"
+    style="z-index: 100"
+    class="smooth-transition visible modal page-display fixed inset-x-0 inset-y-0 bg-gray-400 m-10 p-5 p-0-sm rounded-tl-lg rounded-tr-lg rounded-bl-lg rounded-br-lg shadow-xl"
+  >
+    <button
+      :onclick="openModalMission()"
+      class="fixed top-3 right-3 bg-red-500 hover:bg-red-600 text-white font-bold py-1 px-2 rounded-full inline-flex items-center"
+    >
+      <svg
+        class="fill-current w-4 h-4"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+      >
+        <path
+          d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"
+        />
+      </svg>
+    </button>
+    <div class="justify-center space-x-8">
+      <h1 class="text-3xl font-bold text-center m-4">Our Mission</h1>
+      <p class="text-justify leading-relaxed sm:px-6 p-8 md:px-30 xl:px-40">
+        WHAT IS #3FB00B? #3FB00B is a distinctive Proof of Involvement NFT that
+        serves multiple purposes within the $PEPE Community. This NFT not only
+        symbolizes your membership to #3FB00B but also represents your stake in
+        promoting and raising awareness about the community as a whole. The
+        captivating colors used in the #3FB00B NFT artwork are carefully chosen
+        to reflect and embody the essence and spirit of the $PEPE Community. The
+        vibrant and engaging visuals serve as a visual representation of the
+        community's energy, creativity, and shared values. By owning a #3FB00B
+        NFT, you gain exclusive utilities and opportunities.\n These utilities
+        may include early access to new features, events, or projects within the
+        $PEPE ecosystem. Additionally, as a #3FB00B NFT holder, you become
+        eligible for future utilities and benefits that are specifically
+        designed to enhance your experience and engagement within the community.
+        Being an #3FB00B NFT holder not only grants you access to exciting
+        opportunities but also positions you as an active participant and
+        supporter of the $PEPE Community. Your ownership of this unique NFT
+        demonstrates your dedication and involvement in the community's growth
+        and development. WHY WAS #3FB00B CREATED? #3FB00B was created as a
+        dedicated sub-community within the broader $PEPE ecosystem. Its primary
+        purpose is to foster an environment where passionate $PEPE enthusiasts
+        can come together and actively engage with various artists, creators,
+        and builders. By being a part of #3FB00B, members gain exclusive access
+        to curated Web3 projects, enabling them to discover and support new
+        talent and innovative initiatives. The creation of #3FB00B serves as a
+        platform for the community to explore and embrace the diverse and
+        evolving world of digital art and creativity. Through this
+        sub-community, members have the unique opportunity to connect with
+        emerging artists and creators, discovering their unique perspectives and
+        contributing to their growth and success. By focusing on curated Web3
+        projects, #3FB00B ensures that the initiatives supported are carefully
+        selected and aligned with the ethos and interests of the $PEPE
+        Community. This curated approach not only helps members find exciting
+        and innovative projects but also empowers artists and builders by
+        providing them with a dedicated audience and potential support for their
+        work. Overall, #3FB00B aims to create a thriving ecosystem within the
+        $PEPE Community, fostering collaboration, discovery, and support for
+        artists, creators, and builders in the Web3 space.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+interface Tail {
+  text: string;
+  link: string;
+  class: string;
+  click: Function;
+}
+
+export default defineComponent({
+  data() {
+    return {
+      ABOUT: true,
+      MISSION: false,
+    };
+  },
+  computed: {
+    n() {
+      if (window.innerWidth < 900) {
+        return 21;
+      }
+      if (window.innerWidth < 1200) {
+        return 24;
+      } else {
+        return 50;
+      }
+    },
+    keys(): Array<Tail> {
+      return Array(this.n).fill({ text: null, link: null, class: null });
+    },
+    table() {
+      this.keys[0] = {
+        text: "#3FB00B",
+        link: "",
+        click: this.openModalAbout,
+        class: "square-link",
+      };
+      if (this.n === 21) {
+        this.keys[2] = {
+          text: "BASE & PROOF",
+          link: "",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[6] = {
+          text: "TWITTER",
+          link: "https://twitter.com/pepeisgreen",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[8] = {
+          text: "OPENSEA",
+          link: "https://www.opensea.com",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[10] = {
+          text: "DISCORD",
+          link: "https://discord.com/invite/Vy2rjVAk",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[14] = {
+          text: "MINT",
+          link: "",
+          click: () => null,
+          class: "square-link",
+        };
+        // this.keys[18] = {
+        //   text: "ABOUT",
+        //   link: "", click: () => null,
+        //   class: "square-link open-modal",
+        // };
+      }
+      if (this.n === 24) {
+        this.keys[5] = {
+          text: "BASE & PROOF",
+          link: "",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[2] = {
+          text: "TWITTER",
+          link: "https://twitter.com/pepeisgreen",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[22] = {
+          text: "OPENSEA",
+          link: "https://www.opensea.com",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[17] = {
+          text: "DISCORD",
+          link: "https://discord.com/invite/Vy2rjVAk",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[9] = {
+          text: "MINT",
+          link: "",
+          click: () => null,
+          class: "square-link shadow-inner shadow-2xl",
+        };
+
+        // this.keys[18] = {
+        //   text: "ABOUT",
+        //   link: "", click: () => null,
+        //   class: "square-link open-modal",
+        // };
+      }
+      if (this.n === 50) {
+        this.keys[5] = {
+          text: "APPLY",
+          link: "",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[9] = {
+          text: "BASE & PROOF",
+          link: "",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[14] = {
+          text: "DISCORD",
+          link: "https://discord.com/invite/Vy2rjVAk",
+          click: () => null,
+          class: "square-link",
+        };
+        this.keys[18] = {
+          text: "TWITTER",
+          link: "https://twitter.com/pepeisgreen",
+          click: () => null,
+          class: "square-link",
+        };
+
+        this.keys[26] = {
+          text: "MINT",
+          link: "",
+          click: () => null,
+          class: "square-link shadow-inner shadow-2xl",
+        };
+        this.keys[40] = {
+          text: "MISSION",
+          link: "",
+          click: this.openModalMission,
+          class: "square-link",
+        };
+        this.keys[48] = {
+          text: "OPENSEA",
+          link: "https://www.opensea.com",
+          click: () => null,
+          class: "square-link",
+        };
+      }
+      return this.keys;
+    },
+  },
+  methods: {
+    openModalAbout() {
+      this.ABOUT = !this.ABOUT;
+    },
+    openModalMission() {
+      this.MISSION = !this.MISSION;
+    },
+  },
+});
+</script>
+<style>
+.smooth-transition {
+  transition: opacity 0.5s ease-in-out;
+}
+html,
+body {
+  margin: 0;
+  height: 100%;
+  overflow: hidden;
+}
+
+.grid-container {
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(20%, 1fr));
+  height: 100%;
+  width: 100%;
+  background-color: #4ed419;
+}
+.square {
+  border: 0.05em solid hsl(103, 88%, 45%);
+  background-color: #3fb00b;
+  display: flex;
+}
+.square-link {
+  cursor: pointer;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  transition: transform 0.1s ease-in-out;
+}
+.square:hover {
+  border-color: hsl(103, 87%, 30%);
+  background-color: hsl(103, 87%, 30%);
+}
+.square-link:hover {
+  border-color: hsl(103, 87%, 30%);
+  background-color: hsl(103, 87%, 30%);
+  transform: rotate(20deg);
+}
+.pepe {
+  z-index: 10;
+}
+p {
+  font-family: "Inconsolata", monospace;
+  font-size: 12px;
+  line-height: 10px;
+  color: hsl(103, 95%, 84%);
+}
+h1 {
+  font-family: "Inconsolata", monospace;
+  font-size: 12px;
+  line-height: 10px;
+  color: hsl(103, 95%, 84%);
+}
+@media (min-width: 300px) {
+  .grid-container {
+    grid-template-columns: repeat(3, 1fr);
+  }
+  .square {
+    height: 14.28vh;
+  }
+  .pepe {
+    position: fixed;
+    bottom: 17%;
+    right: 40.4%;
+    width: 50%;
+  }
+}
+@media (min-width: 900px) {
+  .grid-container {
+    grid-template-columns: repeat(6, 1fr);
+  }
+  .square {
+    height: 25vh;
+  }
+  .pepe {
+    position: fixed;
+    bottom: 10%;
+    right: 55.4%;
+    width: 32%;
+  }
+}
+@media (min-width: 1200px) {
+  .grid-container {
+    grid-template-columns: repeat(10, 1fr);
+  }
+  .square {
+    height: 20vh;
+  }
+  .pepe {
+    position: fixed;
+    bottom: 5.2%;
+    right: 50.4%;
+    width: 27%;
+  }
+}
+.modal {
+  overflow-y: scroll;
+  border: 0.2em solid;
+  border-color: hsl(103, 88%, 45%);
+  background-color: #3fb00b;
+  justify-content: center;
+  align-items: center;
+  transition: transform 5s fade-in-out;
+}
+.modal::-webkit-scrollbar {
+  display: none;
+}
+</style>
